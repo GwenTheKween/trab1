8cdeac6b3abdac950550f976abe028008fa3e37b
Ewerton P <ewertonpsa2009@usp.br>
2019-09-28 15:33:07 -0300

    alterado estrutura de entrada

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,21 @@ int main(int argc, char **argv){
 		return 0;
 	}
 	if(argv[1][0] == '1'){//Opcao de criar labirinto manualmente
-		
-	
-	}else if(argv[1][0] == '2'){//Opcao de criar labirinto automaticamente
+		if(argc < 4){
+			printf("faltam parametros, use: %s 1 <altura matriz> <largura matriz>\n", argv[0]);
+			return 0;
+		}
 
+		labirinto l( atoi(argv[2]), atoi(argv[3]));
+		l.gera_labirinto_manual();
+	}else if(argv[1][0] == '2'){//Opcao de criar labirinto automatico
+		if(argc < 4){
+			printf("faltam parametros, use: %s 1 <altura matriz> <largura matriz>\n", argv[0]);
+			return 0;
+		}
 
+		labirinto l( atoi(argv[2]), atoi(argv[3]));
+		l.gera_labirinto_automatico();
 	}else if(argv[1][0] == '3'){//Opcao de abrir um labirinto e rodar um algoritimo sobre ele
 		labirinto l(argv[2]);
 		l.print();
