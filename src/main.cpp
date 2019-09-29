3c332e787d1b72d41b323b240ac2c3733eb0e2fa
EwertonPSA <ewertonpsa2009@usp.br>
2019-09-29 16:05:53 -0300

    alterado estrutura de entrada das funcionalidades

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,23 @@
 #include "labirinto.h"
 
 int main(int argc, char **argv){
+	int height, width;
 	if(argc < 2){
 		printf("uso: %s <arquivo>",argv[0]);
 		return 0;
 	}
-	if(argv[1][0] == '1'){//Opcao de criar labirinto manualmente
-		if(argc < 4){
-			printf("faltam parametros, use: %s 1 <height> <weidth>\n", argv[0]);
-			return 0;
-		}
 
-		labirinto l( atoi(argv[2]), atoi(argv[3]));
-		l.gera_labirinto_manual();
+	if(argv[1][0] == '1'){//Opcao de criar labirinto manualmente
+		printf("digite numero de colunas e linhas\n");
+		scanf("%d %d", &height, &width);
+		labirinto l( height, width);
+		l.gera_labirinto_manual(argv[2]);
 		l.print();
 	}else if(argv[1][0] == '2'){//Opcao de criar labirinto automatico
-		if(argc < 4){
-			printf("faltam parametros, use: %s 1 <height> <width>\n", argv[0]);
-			return 0;
-		}
-
-		labirinto l( atoi(argv[2]), atoi(argv[3]));
-		l.gera_labirinto_automatico();
+		printf("digite numero de colunas e linhas\n");
+		scanf("%d %d", &height, &width);
+		labirinto l( height, width);
+		l.gera_labirinto_automatico(argv[2]);
 	}else if(argv[1][0] == '3'){//Opcao de abrir um labirinto e rodar um algoritimo sobre ele
 		labirinto l(argv[2]);
 		l.print();
