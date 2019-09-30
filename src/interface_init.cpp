08964fff8e55f79b786a0220e2a7dd3c9afa0ced
Lucas <lucastakahashi@usp.br>
2019-09-30 09:41:19 -0300

    inicialização das cores

--- src/interface_init.cpp
+++ src/interface_init.cpp
@@ -1,11 +1,15 @@
 #include "interface_init.h"
 #include <ncurses.h>
+#include <cstdlib>
 
 InterfaceInit::InterfaceInit(){
     initscr();
-    if(has_colors() == TRUE){
-        start_color();
+    if(has_colors() == FALSE){
+        endwin();
+        printf("deu ruim não tem cores aqui\n");
+        exit(1);
     }
+    start_color();
 
 }
 
