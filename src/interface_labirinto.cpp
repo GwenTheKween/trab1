6b0ef28ce9ce6c18ef6581a79cb9a722f7229977
Lucas <lucastakahashi@usp.br>
2019-09-30 09:42:06 -0300

    tentativa de colocar cores
    - ainda estou com problemas para definir cores

--- src/interface_labirinto.cpp
+++ src/interface_labirinto.cpp
@@ -4,24 +4,30 @@
 
 
 InterfaceLabirinto::InterfaceLabirinto(int leftUpX, int leftUpY   , int height , int width , labirinto &lab) : coordLeftUp(leftUpX , leftUpY) , size(width , height) , lab(lab), window(newpad(lab.getHeight() , lab.getWidth()) , WINDOW_desctructor()) {
+
     for(int i =0 ; i < this->lab.getHeight() ; i++){
         wmove(this->window.get() , i , 0);
         auto & linha = this->lab[i];
         for(int j = 0 ; j < this->lab.getWidth() ; j++){
+            //attron(COLOR_PAIR(1));
             char aux = 0;
             if(linha[j] ==  FREE_SPACE){
-                aux = ' ';
+                aux = '*';
             }else if(linha[j] == WALL){
-                addch(ACS_BLOCK);
-                aux='\n';
+                aux=' ';
             }else if(linha[j] == BEGIN){
                 aux = 'o'; // alterar esse daqui depois para alguma cor
             }else if(linha[j] == END){
                 aux = 'X'; // ese daqui também dar alguma cor
             }
-            wprintw(this->window.get(), aux);
+            wprintw(this->window.get(), "%c" , aux);
         }
     }
+    prefresh(this->window.get() ,0 , 0 , leftUpY , leftUpX, leftUpY + height , leftUpX + width );
+    wgetch(this->window.get());
+}
+
+InterfaceLabirinto::~InterfaceLabirinto(){
 }
 
 
