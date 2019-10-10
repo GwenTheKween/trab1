f67bf197c55b8d5fc7441d927b6c435fab7e150f
Lucas <lucastakahashi@usp.br>
2019-10-10 10:29:30 -0300

    definição de comentários para o código da interface

--- src/interface_labirinto.cpp
+++ src/interface_labirinto.cpp
@@ -3,6 +3,8 @@
 #include "ncurses_helpers.h"
 
 
+
+// Outline: Ele define qual caracter deve ser usado e da as caracteríticas padrão do caracter.
 char InterfaceLabirinto::printaCaracter(MAP_INFO value){
     char aux = 0;
     if(value ==  FREE_SPACE){
@@ -11,14 +13,15 @@ char InterfaceLabirinto::printaCaracter(MAP_INFO value){
         wattron(this->window.get() , COLOR_PAIR(BLACK_WHITE));
         aux=' ';
     }else if(value == BEGIN){
-        aux = 'o'; // alterar esse daqui depois para alguma cor
+        aux = 'o';
     }else if(value == END){
-        aux = 'X'; // ese daqui também dar alguma cor
+        aux = 'X';
     }
     wprintw(this->window.get() , "%c" , aux);
     wstandend( this->window.get());
     return aux;
 }
+
 
 
 InterfaceLabirinto::InterfaceLabirinto(int leftUpX, int leftUpY   , int height , int width , labirinto &lab) : coordLeftUp(leftUpX , leftUpY) , size(width , height) , lab(lab), window(newpad(lab.getHeight() + 2 , lab.getWidth() + 2) , WINDOW_desctructor()) {
@@ -28,16 +31,19 @@ InterfaceLabirinto::InterfaceLabirinto(int leftUpX, int leftUpY   , int height ,
 InterfaceLabirinto::~InterfaceLabirinto(){
 }
 
-InterfaceLabirinto::InterfaceLabirinto(const InterfaceLabirinto &outro) : coordLeftUp(outro.coordLeftUp) , size(outro.size) , lab(outro.lab), window(newpad(lab.getHeight() , lab.getWidth()) , WINDOW_desctructor()) , coordLast(outro.coordLast)  {
+InterfaceLabirinto::InterfaceLabirinto(const InterfaceLabirinto &outro) : coordLeftUp(outro.coordLeftUp) , size(outro.size) , lab(outro.lab), window(newpad(lab.getHeight() , lab.getWidth()) , WINDOW_desctructor()) , actualCoord(outro.actualCoord)  {
     this->atualizarMapa();
 }
 
+//Outline: função que desenha o labirinto inteiro no pad. Começa desenhando as bordas e depois cada ponto do labirinto.
 void InterfaceLabirinto::atualizarMapa(){
 
+    // fz a borda
     wattron(this->window.get() , COLOR_PAIR(BLACK_WHITE));
     wborder(this->window.get() , ' ' , ' ', ' ', ' ', ' ', ' ' , ' ' , ' ');
     wattroff(this->window.get() , COLOR_PAIR(BLACK_WHITE));
 
+    // printa todas as posições labirintos.
     for(int i =0 ; i < this->lab.getHeight() ; i++){
         wmove(this->window.get() , i+1 , 1);
         auto & linha = this->lab[i];
@@ -45,42 +51,51 @@ void InterfaceLabirinto::atualizarMapa(){
             //attron(COLOR_PAIR(1));
             this->printaCaracter(linha[j]);
             if(linha[j] == BEGIN){
-                this->coordLast = std::make_pair(j+1 , i+1);
+                this->actualCoord = std::make_pair(j+1 , i+1);
             }
         }
     }
 
 
-    this->refresh();
     wgetch(this->window.get());
 }
 
+// Outline: Define a posição atual do labirinto. E printa o caracter, não atualiza a tela, necessário chamar
+// InterfaceLabirinto::refresh().
 void InterfaceLabirinto::definiPosicao(int y , int x  ,COLOR_MAPS color ){
     wattron(this->window.get() , COLOR_PAIR(color));
     wmove(this->window.get() , y+1 ,x+1);
-    this->coordLast.first = x;
-    this->coordLast.second = y;
+    this->actualCoord.first = x;
+    this->actualCoord.second = y;
     this->printaCaracter(this->lab[y][x]);
-    this->refresh();
     wgetch(this->window.get());
 }
 
+//Outline: define a parte da tela que via ser mostrada tentando manter a posição atual no centro da tela,
+//definindo qual posição do labirinto vai ficar no canto
+//superior esquerdo da janela, depois disso só define os outros pontos apartir do tamanho da janela em
+//que vai ser exibido o labirinto.
 void InterfaceLabirinto::refresh() {
+    // variaveis que são usadas para  centralizar a posição atual.
     int mh = this->size.second / 2;
     int mw = this->size.first / 2;
     int cornerLeUpY = 0;
     int cornerLeUpX = 0;
-    if((this->lab.getWidth()+2 > this->size.first) && (this->coordLast.first - mw > 0)){
-        cornerLeUpX = this->coordLast.first - mw;
-        if((cornerLeUpX + this->size.first ) > (this->lab.getWidth()+2)){
+    // aqui faz a verificação se na coordenada x atual está num ponto ond enão é possível centralizar porque
+    // ele está muito proximo da origem, e depois verifica se está muito proximo do fim do labirinto
+    if((this->lab.getWidth()+2 > this->size.first) && (this->actualCoord.first - mw > 0)){//verificação se está no inicio
+        cornerLeUpX = this->actualCoord.first - mw;
+        if((cornerLeUpX + this->size.first ) > (this->lab.getWidth()+2)){ // verificação se está no final.
             cornerLeUpX = this->lab.getWidth() - this->size.first + 2;
         }
     }
-    if( (this->lab.getHeight()+2 > this->size.second) && (this->coordLast.second - mh > 0) ){
-        cornerLeUpY = this->coordLast.second - mh;
+    // repete a mesma coisa da coordenada x só que para y.
+    if( (this->lab.getHeight()+2 > this->size.second) && (this->actualCoord.second - mh > 0) ){
+        cornerLeUpY = this->actualCoord.second - mh;
         if((cornerLeUpY + this->size.second) > (this->lab.getHeight() + 2)){
             cornerLeUpY -= this->lab.getHeight() - this->size.second + 2;
         }
     }
+    // faz a atualização do pad para escrever na janela.
     prefresh(this->window.get() , cornerLeUpY , cornerLeUpX , this->coordLeftUp.second , this->coordLeftUp.first , this->coordLeftUp.second + this->size.second  -1, this->coordLeftUp.first + this->size.first - 1);
 }
--- src/interface_labirinto.h
+++ src/interface_labirinto.h
@@ -7,33 +7,68 @@
 #include "labirinto.h"
 #include "interface_init.h"
 
+// Classe que encapsula a interface que irá mostrar o labirinto.
 
 class InterfaceLabirinto {
-    // esse argumento é para especificar a posição do labirinto na tela.
+    // esse argumento é para especificar a posição do canto superior esquerdo do labirinto, as coordenada
+    // está na forma (x,y).
     std::pair<int, int> coordLeftUp;
-    // esse é para especificar o tamanho da janela.
+    // esse é para especificar o tamanho da janela, Está na forma (width,height).
     std::pair<int, int> size;
     // esse armazena o labirinto que está sendo mostrado.
     labirinto &lab;
-    // esse daqui é a janela do ncurses.
+    // esse daqui é a janela do ncurses. É utilizado o pad que permite janelas maiores do que a tela
+    // dessa forma pode desenhar um labirinto maior que a tela e especificar a parte que vai ser mostrado.
     std::unique_ptr<WINDOW , WINDOW_desctructor> window;
     
-    std::pair<int , int > coordLast;
+    // marca a posição atual no labirinto,(x,y).
+    std::pair<int , int > actualCoord;
 
     private:
 
+    //função que cuida de printar o caracter.
     char printaCaracter(MAP_INFO value);
 
     public:
 
+    /* construtor da interface do labirinto, precisa da posição em que o labirinto vai ser posicionado e do tamanho da tela
+     * em que ele vai estar, mais o labirinto que ele vai mostrar.
+     *
+     * Argumentos:
+     *  leftUpX -> coordenada x onde vai ficar o canto superior esquerdo da interface.
+     *  leftUpY -> coordenada y onde via ficar o canto superior esquerdo da interface.
+     *  height  -> altura da tela.
+     *  width   -> largura da tela.
+     *  lab     -> labirinto que vai ser mostrado.
+     */
     InterfaceLabirinto(int leftUpX, int leftUpY   , int height , int width , labirinto &lab);
+
+    /*
+     * Construtor que faz a copia da interface. Ele cria uma nova janela.
+     */
     InterfaceLabirinto(const InterfaceLabirinto& outro);
+
+    // destrutor
     ~InterfaceLabirinto();
 
+    /*
+     * Função que define a posição atual do labirinto
+     *
+     * Argumentos:
+     *  y       -> posição y atual do labirinto.
+     *  x       -> posição x atual do labirinto.
+     *  color   -> cor em que vai ser pintado a posição atual.
+     */
     void definiPosicao(int y , int x , COLOR_MAPS color);
 
+    /*
+     * Função para rescrever o mapa inteiro. (não marca os lugares visitados).
+     */
     void atualizarMapa();
 
+    /*
+     * Função para escrever o labirinto na tela de verdade.
+     */
     void refresh();
 
 
