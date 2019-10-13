77bd2035022bf99e4144c109ef14bd7c8ada261c
EwertonPSA <ewertonpsa2009@usp.br>
2019-10-13 14:10:28 -0300

    corrigido a busca

--- src/best_first_search.cpp
+++ src/best_first_search.cpp
@@ -1,7 +1,9 @@
 #include "best_first_search.h"
+#define VISITED 1
+#define NOT_VISITED 0
 
-bool Best_first::check_start_end_wall(int column, int line){
-	return (map[column][line] == END || map[column][line] == BEGIN || map[column][line] == WALL);
+bool Best_first::check_start_wall(int column, int line){
+	return (map[column][line] == BEGIN || map[column][line] == WALL);
 }
 
 Best_first::~Best_first(){
@@ -10,9 +12,11 @@ Best_first::~Best_first(){
 		free(enderecosUsados[i]);//libera os enderecos
 	}
 	enderecosUsados.clear();
+	seqVisitados.clear();
+	percursoLabirinto.clear();
 }
 
-void Best_first::inclui_proximo_vertice(int nextColumn, int nextLine, int weight, no_t* father,
+void Best_first::inclui_proximo_vertice(int nextColumn, int nextLine, double weight, no_t* father,
 		std::priority_queue<no_t*, std::vector<no_t*>, compare_best_first>& caminhos){
 	no_t* nextVert = (no_t*)malloc(sizeof(no_t));
 	nextVert->column = nextColumn;
@@ -45,24 +49,27 @@ void Best_first::setCaminhoLabirinto(no_t* origem){
 	}
 }
 
-void libera_Enderecos_Marca_visitados(std::deque<std::pair<int,int>>& seqVisitados, std::vector<no_t*>& enderecosUsados){
-	}
-
 
 std::vector<std::pair<int,int>> Best_first::executar(){ 
 	std::priority_queue<no_t*, std::vector<no_t*>, compare_best_first> caminhos;
-	labirinto mapVisited = map;
 
 	double time;
 	char flagUp, flagDown, flagLeft, flagRight;
 	int height = map.getHeight(), width = map.getWidth();
+	char mapVisited[height][width];
 	no_t* vert;
+
+	for(int i = 0; i < height; i++){
+		for(int j = 0; j < width; j++){
+			mapVisited[i][j] = NOT_VISITED;
+		}
+	}
 
 	inclui_proximo_vertice(map.getColumnBegin(), map.getLineBegin(), 0, NULL, caminhos);
 	while(!caminhos.empty()){
 		vert = caminhos.top();
 		caminhos.pop();
-
+		mapVisited[vert->column][vert->line] = VISITED;
 		if(map[vert->column][vert->line] != END){
 			flagUp = (vert->column+1 < height);
 			flagDown = (vert->column-1 >= 0);
@@ -70,39 +77,37 @@ std::vector<std::pair<int,int>> Best_first::executar(){
 			flagRight = (vert->line+1 < width);
 
 			//Uso ERROR como posicao visitada
-			if(flagUp && !check_start_end_wall(vert->column+1, vert->line) 
-					&& mapVisited[vert->column+1][vert->line] != ERROR){
+			if(flagUp && !check_start_wall(vert->column+1, vert->line) 
+					&& mapVisited[vert->column+1][vert->line] != VISITED){
 				inclui_proximo_vertice(vert->column+1, vert->line, 1, vert, caminhos);
-
-				mapVisited[vert->column+1][vert->line] = ERROR;//Marca posicao como visitado
-			}else if(flagDown && !check_start_end_wall(vert->column-1, vert->line) 
-					&& mapVisited[vert->column-1][vert->line] != ERROR){
+			}
+			if(flagDown && !check_start_wall(vert->column-1, vert->line) 
+					&& mapVisited[vert->column-1][vert->line] != VISITED){
 				inclui_proximo_vertice(vert->column-1, vert->line, 1, vert, caminhos);
-				mapVisited[vert->column-1][vert->line] = ERROR;//Marca posicao como visitado
-			}else if(flagRight && !check_start_end_wall(vert->column, vert->line+1)
-					&& mapVisited[vert->column][vert->line+1] != ERROR){
+			}
+			if(flagRight && !check_start_wall(vert->column, vert->line+1)
+					&& mapVisited[vert->column][vert->line+1] != VISITED){
 				inclui_proximo_vertice(vert->column, vert->line+1, 1, vert, caminhos);
-				mapVisited[vert->column][vert->line+1] = ERROR;
-			}else if(flagLeft && !check_start_end_wall(vert->column, vert->line-1)
-					&& mapVisited[vert->column][vert->line-1] != ERROR){
+			}
+			if(flagLeft && !check_start_wall(vert->column, vert->line-1)
+					&& mapVisited[vert->column][vert->line-1] != VISITED){
 				inclui_proximo_vertice(vert->column, vert->line-1, 1, vert, caminhos);
-				mapVisited[vert->column][vert->line-1] = ERROR;
-			}else if(flagUp && flagRight && !check_start_end_wall(vert->column+1, vert->line+1)
-					&& mapVisited[vert->column+1][vert->line+1] != ERROR){
+			}
+			if(flagUp && flagRight && !check_start_wall(vert->column+1, vert->line+1)
+					&& mapVisited[vert->column+1][vert->line+1] != VISITED){
 				inclui_proximo_vertice(vert->column+1, vert->line+1, sqrt(2), vert, caminhos);
-				mapVisited[vert->column+1][vert->line+1] = ERROR;
-			}else if(flagUp && flagLeft && !check_start_end_wall(vert->column+1, vert->line-1)
-					&& mapVisited[vert->column+1][vert->line-1] != ERROR){
+			}
+			if(flagUp && flagLeft && !check_start_wall(vert->column+1, vert->line-1)
+					&& mapVisited[vert->column+1][vert->line-1] != VISITED){
 				inclui_proximo_vertice(vert->column+1, vert->line-1, sqrt(2), vert, caminhos);
-				mapVisited[vert->column+1][vert->line-1] = ERROR;
-			}else if(flagDown && flagRight && !check_start_end_wall(vert->column-1, vert->line+1)
-					&& mapVisited[vert->column-1][vert->line+1] != ERROR){
+			}
+			if(flagDown && flagRight && !check_start_wall(vert->column-1, vert->line+1)
+					&& mapVisited[vert->column-1][vert->line+1] != VISITED){
 				inclui_proximo_vertice(vert->column-1, vert->line+1, sqrt(2), vert, caminhos);
-				mapVisited[vert->column-1][vert->line+1] = ERROR;
-			}else if(flagDown && flagLeft && !check_start_end_wall(vert->column-1, vert->line-1)
-					&& mapVisited[vert->column-1][vert->line-1] != ERROR){
+			}
+			if(flagDown && flagLeft && !check_start_wall(vert->column-1, vert->line-1)
+					&& mapVisited[vert->column-1][vert->line-1] != VISITED){
 				inclui_proximo_vertice(vert->column-1, vert->line-1, sqrt(2), vert, caminhos);
-				mapVisited[vert->column-1][vert->line-1] = ERROR;
 			}
 		}else if(map[vert->column][vert->line] == END){
 			setCaminhoLabirinto(vert);//Coloca em percursoLabirinto o caminho ateh a saida
