9f713805ad0909281a6877b1c66c8447c83baa04
Lucas <lucastakahashi@usp.br>
2019-10-14 13:57:45 -0300

    comentários e correção do caminho gerado pela bfs

--- src/bfs.cpp
+++ src/bfs.cpp
@@ -13,17 +13,28 @@ const std::deque<std::pair<int,int>> &bfsSearch::getSequenciaDeVisitados() const
     return this->visitados;
 }
 
+// BFS nao tem mais palavras
 std::vector<std::pair<int,int> > bfsSearch::executar(){
+    // vetor que vai ter a resposta.
     std::vector<std::pair<int,int> > resp;
+    // fila da bfs
     std::queue<std::pair < int , int > > fila;
+    // auxiliar de posição invpalida
     std::pair<int ,int> auxNaoVisitado(-1,-1);
+    // mapa que vai gerar o caminho percorrido.
     std::vector<std::vector< std::pair<int , int > > > mapaVisitados(this->map->getHeight() , std::vector<std::pair<int , int> >(this->map->getWidth() , auxNaoVisitado));
+    // posição atual da busca.
     std::pair<int,int> atual(this->map->getStart());
+    // coordenada do inicio
     std::pair<int , int> inicio(atual);
+    // coordenada da coordenada final
     std::pair<int,int> fim(this->map->getEnd());
+    // marca que o atual chegou pelo atual. para não andar de novo aqui
     mapaVisitados[atual.first][atual.second] = atual;
     while(atual != fim){
         this->visitados.push_back(atual);
+        // cria todas as coordenadas em volta e depois verifica quais são validas, que não foram visitadas e não é parede
+        // só asism ele coloca na fila de busca.
         std::vector<std::pair<int , int> > auxFilhos(8 , {0,0});
         auxFilhos[0] = {atual.first-1 , atual.second};
         auxFilhos[1] = {atual.first-1 , atual.second-1};
@@ -46,10 +57,12 @@ std::vector<std::pair<int,int> > bfsSearch::executar(){
         atual = fila.front();
         fila.pop();
     }
+    // cria o vetor de resposta.
     while(atual != inicio){
         resp.push_back(atual);
         atual = mapaVisitados[atual.first][atual.second];
     }
+    resp.push_back(inicio);
     return resp;
 }
 
