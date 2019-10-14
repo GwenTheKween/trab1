c7af8a79ab450d0427ed64bc93e0d8dc8300718d
billionai <larsen.bruno.p@gmail.com>
2019-10-14 09:04:43 -0300

    added get functions for start and end, and improved documentation

--- src/labirinto.h
+++ src/labirinto.h
@@ -20,6 +20,8 @@ class labirinto{
 	std::pair<int, int> begin, end;
 	std::vector<std::vector<MAP_INFO> > map;
 private:
+	//Metodo que muda o tamanho do mapa, usando resize nos vectors.
+	//Foi feito para ser usado apenas na inicializacao da classe
 	void resize(int h,int w);
 
 
@@ -27,10 +29,22 @@ private:
 	int check_num_wall(int column, int line);
 
 	bool check_start_end(int column, int line);
+
+	//adiciona a parede gerada pelo algoritmo nova_geracao, e retorna ela para o algoritmo
+	parede create_wall(std::pair<int, int> start, std::pair<int, int> end);
+
+	//Seta o inicio do mapa como a coordenada passada, e armazena essa coordenada
+	void setStart(std::pair<int, int> coord);
+
+	//Seta o fim do mapa como a coordenada passada, e armazena essa coordenada
+	void setEnd(std::pair<int,int> coord);
 public:
 
+	//Construtor da classe, chama o metodo de resize e prepara para gerar aleatoriamente ou pelo usuario
 	labirinto(int h, int w);
+	//Construtor da classe, chama resize e depois le do arquivo passado
 	labirinto(char* filename);
+	//Destrutor da classe, zera os valores que nao tem o proprio destrutor
 	~labirinto(){
 		height = 0;
 		width = 0;
@@ -39,10 +53,12 @@ public:
 	/*Funcao que escreve o labirinto criado em um arquivo */
 	void write_labirinto(const char* filename);
 
+	//operador para poder ler o mapa fora da classe, mas nao pode editar
 	std::vector<MAP_INFO> operator [](int i){
 		return map[i];
 	}
 
+	//operador para poder ler o mapa sem edita-lo, usando pairs para facilitar a passagem de coordenadas
 	int operator [](std::pair<int, int> p){
 		return map[p.first][p.second];
 	}
@@ -50,16 +66,22 @@ public:
 	void gera_labirinto_manual(char* filename);
 	void gera_labirinto_automatico(char* filename);
 
+	//Geracao aleatoria do mapa, recebe a quantidade de paredes a ser criada.
+	//O algoritmo escolhe alguma parede existente, e em algum ponto dessa parede cria uma outra parede, ortogonal a original
 	void nova_geracao(int wallCount);
 
     int getHeight();
     int getWidth();
+	//Retorna se a coordenada passada esta livre
 	bool isFree(std::pair<int,int> coord);
+	//Checa se existem paredes ao redor do ponto passado
 	bool wallsAround(std::pair<int,int> coord);
 
-	void setStart(std::pair<int, int> coord);
-	void setEnd(std::pair<int,int> coord);
-	parede create_wall(std::pair<int, int> start, std::pair<int, int> end);
+	//Retorna a coordenada de inicio
+	std::pair<int, int> getStart(){return begin;}
+
+	//Retorna a coordenada de fim
+	std::pair<int, int> getEnd() {return end;}
 
 	void print();
 };
